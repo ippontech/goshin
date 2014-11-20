f3ca7bf6c13da18e70d28185dfe87c1f9b7a8ddb
Pierre-Alain Riviere <pariviere@ippon.fr>
2014-11-20 16:16:54 +0400

    Gorilla aims is to be a dropped-in alternative to riemann-health without dependencies hell

---
+++ cpu.go
@@ -0,0 +1,51 @@
+package gorilla
+
+import (
+        "fmt"
+        "os/exec"
+    )
+import linuxproc "github.com/c9s/goprocinfo/linux"
+
+type CPUTime struct {
+        last, actual linuxproc.CPUStat
+}
+
+func (c *CPUTime) Store() {
+        c.last = c.actual
+        stat,_ := linuxproc.ReadStat("/proc/stat")
+        c.actual =  stat.CPUStatAll
+}
+
+func (c *CPUTime) Used() uint64 {
+        var used =  (c.actual.User + c.actual.Nice + c.actual.System) -  (c.last.User + c.last.Nice + c.last.System)
+
+        return used
+}
+
+func (c *CPUTime) Total() uint64 {
+        return c.Used() + c.actual.Idle - c.last.Idle
+}
+
+func (c* CPUTime) Usage() float64 {
+        var fraction float64 = float64(c.Used()) / float64(c.Total())
+        return fraction
+}
+
+func (c* CPUTime) Ranking() string {
+        out, _ :=  exec.Command("sh", "-c", "ps -eo pcpu,pid,comm | sort -nrb -k1 | head -10").Output()
+
+        s := string(out[:])
+
+       return fmt.Sprint("user+nice+system\n\n", s)
+}
+
+func (c* CPUTime) Report(f func(*Metric)) {
+        c.Store()
+        m := new(Metric)
+
+        m.service = "cpu"
+        m.value = c.Usage()
+        m.description = c.Ranking()
+
+        f(m)
+}
---
+++ gorilla.go
@@ -0,0 +1,49 @@
+package gorilla
+
+import (
+        "fmt"
+        "time"
+        "github.com/bigdatadev/goryman"
+    )
+
+
+type Metric struct {
+        service, description string
+        value interface {}
+}
+
+type Gorilla struct {
+        Address string
+        CheckCPU bool
+}
+
+func Instance() *Gorilla {
+        return &Gorilla{}
+}
+
+
+func (g *Gorilla) Start() {
+        fmt.Print("Gare aux gorillllles!\n")
+
+        reporter := func (metric *Metric) {
+
+                c := goryman.NewGorymanClient(g.Address)
+                c.Connect()
+
+                c.SendEvent(&goryman.Event{
+                        Metric: metric.value,
+                        Ttl: 10,
+                        Service: metric.service,
+                        Description: metric.description,
+                        State: "ok"})
+
+                defer c.Close()
+        }
+
+        cputime := new (CPUTime)
+
+        for i := 0 ; i < 1000 ; i++ {
+                cputime.Report(reporter)
+                time.Sleep(5 * time.Second)
+        }
+}
